e90266c8e9bdc7773c1dc260cbb27fba70cf7719
gabrielm407 <moffettgabriel@gmail.com>
2025-10-03 17:36:05 -0700

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CiCdPipelineDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, CI/CD Pipeline Demo!");
+        }
+    }
+}
