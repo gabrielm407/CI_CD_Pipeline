2fdec7d2062a7742bae0031bcd1b0cf82433fae0
gabrielm407 <moffettgabriel@gmail.com>
2025-10-05 08:01:41 -0700

    C# fail code

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Data.SqlClient;
 
 namespace CiCdPipelineDemo
 {
@@ -7,6 +8,17 @@ namespace CiCdPipelineDemo
         static void Main(string[] args)
         {
             Console.WriteLine("CI/CD pipeline successful!");
+
+            // BAD: Vulnerable SQL query (CodeQL will flag this)
+            string userInput = "1 OR 1=1";
+            string query = "SELECT * FROM Users WHERE Id = '" + userInput + "'";
+
+            using (var connection = new SqlConnection("Server=.;Database=Test;Trusted_Connection=True;"))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                command.ExecuteReader();
+            }
         }
     }
 }
